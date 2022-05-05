ffb7ec53acfcc14ea6545a93dce66d7c8d8f0432
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-05-05 13:02:19 -0700

    Add project files.

---
+++ BookFun/Book.cpp
@@ -0,0 +1,36 @@
+#include "Book.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Book::Book(string author, string title, string genre, int numPages) {
+	this->author = author;
+	this->title = title;
+	this->genre = genre;
+	this->numPages = numPages;
+}
+
+string Book::getAuthor() const
+{
+	return author;
+}
+
+string Book::getTitle() const
+{
+	return title;
+}
+
+string Book::getGenre() const
+{
+	return genre;
+}
+
+int Book::getNumPages() const
+{
+	return numPages;
+}
+
+void Book::printBookDetails() const
+{
+	cout << title << " by " << author << " has " << numPages << " pages, " << " and it's genre is " << genre << endl;
+}
---
+++ BookFun/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Book.h"
+using namespace std;
+
+//void printBookDetails(const Book& book);
+
+int main()
+{
+	Book gameOfThrones("George Martin", "Game of Thrones", "Fantasy", 864);
+
+	Book mathBook("James Stewart", "Calculus", "Math", 1392);
+
+	Book cpp("Bjarne Stroustrup", "The C++ Programming Language", "Programming", 1376);
+
+	gameOfThrones.printBookDetails();
+	mathBook.printBookDetails();
+	cpp.printBookDetails();
+
+	return 0;
+}
+
+//void printBookDetails(const Book& book)
+//{
+//	cout << book.getTitle() << " by " << book.getAuthor() << " has " << book.getNumPages() << " pages, " << " and it's genre is " << book.getGenre() << endl;
+//}
